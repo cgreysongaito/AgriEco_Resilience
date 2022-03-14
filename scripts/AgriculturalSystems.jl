e3042b9c67d816459bc34459960954131f835d57
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2022-03-14 17:58:32 -0400

    paying around with geometric approach to setting parameters

--- scripts/AgriculturalSystems.jl
+++ scripts/AgriculturalSystems.jl
@@ -1,14 +1,14 @@
 include("packages.jl")
 @vars C
-@var y y₀
+@vars y y₀
 
 f(C) = y * C / (y₀ + C)
 SymPy.simplify(diff(f(C),C))
 
 #Monod function (without critical value)
 @with_kw mutable struct MonodPar
-    r = 0.1
-    k1 = 0.5
+    r = 1.0
+    k1 = 1.0
 end
 
 function monod(N, p)
@@ -25,63 +25,66 @@ end
 
 let 
     Nrange = 0.0:0.01:5.0
-    data = [monod(N,MonodPar(r=1.0)) for N in Nrange]
-    data2 = [monod_diff(N,MonodPar(r=1.0)) for N in Nrange]
+    data = [monod(N,MonodPar(r=0.5,k1 = 1.0)) for N in Nrange]
+    # data2 = [monod_diff(N,MonodPar(r=1.0)) for N in Nrange]
     test = figure()
     plot(Nrange, data)
-    plot(Nrange, data2)
+    ylim(0.0,1.0)
+    xlim(0.0, 5.0)
+    # plot(Nrange, data2)
     return test
-end
-
-function monod_plot(gro, k1val)
-    par = MonodPar(r = gro, k1 = k1val)
-    plot!(N -> monod(N, par), 0, 10, label = "r $gro k1 $k1val")
-end
-
-let
-    plot()
-    monod_plot(0.5, 0.5)
-    monod_plot(0.5, 0.9)
-    monod_plot(0.5, 3)
-end
-
-let
-    plot()
-    monod_plot(0.5, 0.5)
-    monod_plot(0.8, 0.5)
-    monod_plot(1, 0.5)
 end
 
 # r changes where the top plateua occurs but not shape. k1 changes shape ie gradient
 
-#Monod function (with critical value)
 
-@with_kw mutable struct MonodCritPar
-    r = 0.1
-    k1 = 0.5
-    nc = 0.4
-end
-
-function monodcrit(N, p)
-    @unpack r, k1, nc = p
-    r * ( N - nc ) / ( k1 + ( N - nc ) )
-end
-
-function monodcrit_plot(gro, k1val, ncval)
-    par = MonodCritPar(r = gro, k1 = k1val, nc = ncval)
-    plot(N -> monodcrit(N, par), 0, 10, label = "r $gro k1 $k1val nc $ncval")
-end
-
-monodcrit_plot(0.5, 0.5, 0.5)
-monodcrit_plot(0.5, 0.5, 0.4)
-monodcrit_plot(0.5, 0.5, 0.1)
-
-let
-    plot()
-    monod_plot(0.5, 0.5)
-    monod_plot(0.8, 0.5)
-    monod_plot(1, 0.5)
-end
 
 #note for model - Can vary r depending on the amount of water within a year. Keep k1 larger so that slope is smaller.
 #white and red noise can be created by an AR process – equation 1 in ruokolainen et al 2009
+
+
+# Yield model
+@with_kw mutable struct BMPPar
+    ymax = 1.0
+    y0 = 1.0
+    p = 1.0
+    c = 1.0
+end
+
+function yield(I, par)
+    @unpack ymax, y0 = par
+    return ymax * I / (y0 + I)
+end
+
+function margprod(I, par)
+    @unpack ymax, y0 = par
+    return ymax * y0 / (( y0 + I )^2)
+end
+
+function maxprofit_vals(par)
+    @unpack ymax, y0, p, c = par
+    I = y0 * ((((p^2)*(ymax^2)*y0) / c) -1)
+    Y = yield(I, ymax, y0)
+    return [I, Y]
+end
+
+#Any way to geometrically show the parameters and their effects
+#Marginal revenue versus marginal cost figures
+function margcost(I, par)
+    @unpack c = par
+    return c / margprod(I, par)
+end
+
+let 
+    Irange = 0.0:0.01:10.0
+    Yield = [yield(I, BMPPar(ymax=5)) for I in Irange]
+    MC = [margcost(I, BMPPar(ymax=5)) for I in Irange]
+    test = figure()
+    plot(Yield, MC)
+    hlines(5, 0.0, 5.0)
+    return test
+end
+
+
+
+
