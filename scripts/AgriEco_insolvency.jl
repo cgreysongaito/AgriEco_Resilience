c876f06f325b432555d1ec5e7dc7dc25549483f5
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2022-11-22 11:57:55 -0500

    adding ideas for insolvency simulation

---
+++ scripts/AgriEco_insolvency.jl
@@ -0,0 +1,34 @@
+
+
+## Starting values
+#Land value plus machinery
+#Debt
+#what is the starting value of liquid assets? (equity)
+
+#data set of year, illiquid and liquid assets, liabilities, amortized debt amount
+
+#each year
+#liquid assets minus input costs  QUESTION what to set input at - do I make inputs just match rest of liquid assets or add more debt)
+#liquid assets minus minus amortized liabilities
+#liabilities minus ammortized liabilities
+# if liquid assets go into negatives then add new debt to replace negative (liabilities plus new debt)
+#if liabilities increase recalculate amortized debt payments
+#run profits function and add to liquid assets
+
+
+function profits()
+
+end
+
+function amortization_recal(liabilities, years_left, interest)
+
+end
+
+function liabilities_update(currentliabilities)
+
+end
+
+function liquidassets_update(currentassets)
+
+end
+
