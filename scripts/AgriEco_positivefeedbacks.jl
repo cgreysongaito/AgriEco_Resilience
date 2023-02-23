cd4ec7265eb4fa034aeffe0f1b802ccadb296213
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2023-02-23 16:17:30 -0500

    working on expected terminal assets analysis, realised problem with rev/exp parameter setting

--- scripts/AgriEco_positivefeedbacks.jl
+++ scripts/AgriEco_positivefeedbacks.jl
@@ -83,8 +83,10 @@ function whitenoise_createdata(noiselocation, basepar, noisepar, maxyears, seed)
         return hcat(1:1:maxyears, repeat([inputsyield[2]],maxyears), repeat([inputsyield[1]],maxyears), pricedisturbed_white(basepar, noisepar, maxyears, seed), repeat([basepar.c],maxyears))
     elseif noiselocation == "cost"
         return hcat(1:1:maxyears, repeat([inputsyield[2]],maxyears), repeat([inputsyield[1]],maxyears), repeat([basepar.p],maxyears), costdisturbed_white(basepar, noisepar, maxyears, seed))
-    elseif noiselocaiton == "all"
+    elseif noiselocation == "all"
         return hcat(1:1:maxyears, yielddisturbed_white(inputsyield, noisepar, maxyears, seed), repeat([inputsyield[1]],maxyears), pricedisturbed_white(basepar, noisepar, maxyears, seed), costdisturbed_white(basepar, noisepar, maxyears, seed))
+    elseif noiselocation == "none"
+        return hcat(1:1:maxyears, repeat([inputsyield[2]],maxyears), repeat([inputsyield[1]], maxyears), repeat([basepar.p],maxyears), repeat([basepar.c],maxyears))
     else
         error("Check the variable noiselocation")
     end
@@ -134,9 +136,29 @@ function whitenoise_distribution(noiselocation, basepar, noisepar, interestpar, 
     return assetsdebtdata
 end
 
-calc_y0(1.33,174.0,139,6.70)
+function distribution_prob_convert(histogramdata)
+    sumcounts = sum(histogramdata.weights)
+    probdata = zeros(length(histogramdata.weights))
+    for bini in eachindex(probdata)
+        probdata[bini] = histogramdata.weights[bini]/sumcounts
+    end
+    return probdata
+end
 
-whitenoise_createdata("yield", FarmBasePar(), NoisePar(), 50, 1)
+function expectedterminalassets(distributiondata, numbins)
+    histogramdata = fit(Histogram, distributiondata, nbins=numbins)
+    probdata = distribution_prob_convert(histogramdata)
+    expectedassets = 0
+    for bini in eachindex(probdata)
+        midpoint = histogramdata.edges[1][bini]+step(histogramdata.edges[1])/2
+        expectedassets += midpoint*probdata[bini]
+    end
+    return expectedassets
+end
+
+expectedterminalassets(whitenoise_distribution("yield", FarmBasePar(), NoisePar(), InterestPar(), 50, 1000), 25)
+
+
 
 let 
     data = whitenoise_distribution("yield", FarmBasePar(), NoisePar(), InterestPar(), 50, 1000)
@@ -145,18 +167,80 @@ let
     return test
 end
 
+function exptermassets_revexpcon_data(noiselocation, revexpratio, ymaxrange, noisepar, interestpar, maxyears, reps, numbins, pval::Float64 =  6.70, cval::Float64 = 139.0)
+    data=zeros(length(ymaxrange), 2)
+    @threads for ymaxi in eachindex(ymaxrange)
+        y0val = calc_y0(revexpratio, ymaxrange[ymaxi], cval, pval)
+        par = FarmBasePar(ymax = ymaxrange[ymaxi], y0 = y0val, c = cval, p = pval)
+        distributiondata = whitenoise_distribution(noiselocation, par, noisepar, interestpar, maxyears, reps)
+        data[ymaxi, 1] = ymaxrange[ymaxi]
+        data[ymaxi, 2] = expectedterminalassets(distributiondata, numbins)
+    end
+    return data
+end 
+#do I need if minimum(filter(!isnan, [margcostIII(I, par) for I in Irange])) >= par.p #|| minimum(filter(!isnan, [avvarcostIII(I, par) for I in Irange])) >= par.p
 
-let
-    data = distribution("yield_disturbance", FarmBasePar(ymax=174.0, y0=15.0), NoisePar(yielddisturbed_CV=0.05, yielddisturbed_r=0.9), InterestPar(), 50, 50)
+let 
+    data133 = exptermassets_revexpcon_data("yield", 1.33, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
+    data110 = exptermassets_revexpcon_data("yield", 1.10, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
+    data100 = exptermassets_revexpcon_data("yield", 1.00, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
+    data095 = exptermassets_revexpcon_data("yield", 0.95, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
     test = figure()
-    plt.hist(data, 25)
+    plot(data133[:,1], data133[:,2], color="blue", label="Rev/Exp = 1.33")
+    plot(data110[:,1], data110[:,2], color="red", label="Rev/Exp = 1.10")
+    plot(data100[:,1], data100[:,2], color="purple", label="Rev/Exp =1.00")
+    plot(data095[:,1], data095[:,2], color="orange", label="Rev/Exp = 0.95")
+    xlabel("Ymax")
+    ylabel("Expected terminal assets")
+    legend()
     return test
 end
 
-
-let
-    data = distribution("price_disturbance", FarmBasePar(), NoisePar(p_CV=0.2,p_r=0.9), InterestPar(), 50, 50)
+let 
+    data133 = exptermassets_revexpcon_data("none", 1.33, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
+    data110 = exptermassets_revexpcon_data("none", 1.10, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
+    data100 = exptermassets_revexpcon_data("none", 1.00, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
+    data095 = exptermassets_revexpcon_data("none", 0.95, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
     test = figure()
-    plt.hist(data, 25)
+    plot(data133[:,1], data133[:,2], color="blue", label="Rev/Exp = 1.33")
+    plot(data110[:,1], data110[:,2], color="red", label="Rev/Exp = 1.10")
+    plot(data100[:,1], data100[:,2], color="purple", label="Rev/Exp =1.00")
+    plot(data095[:,1], data095[:,2], color="orange", label="Rev/Exp = 0.95")
+    xlabel("Ymax")
+    ylabel("Expected terminal assets")
+    legend()
+    return test
+end
+
+#why is yield, price, and cost basically the same result?**** No noise anywhere makes the same figure
+calc_y0(1.33, 174, 139.0, 6.70)
+calc_y0(1.33, 120, 139.0, 6.70)
+
+inputsyield1 = maxprofitIII_vals(FarmBasePar(ymax=174.0, y0=calc_y0(1.33, 174, 139.0, 6.70), p=6.70, c=139.0))
+rev1 = revenue_calc(inputsyield1[2], 6.70)
+exp1 = expenses_calc(inputsyield1[1], 139.0)
+rev1/exp1
+rev1-exp1
+
+inputsyield2 = maxprofitIII_vals(FarmBasePar(ymax=120.0, y0=calc_y0(1.33, 120.0, 139.0, 6.70), p=6.70, c=139.0))
+rev2 = revenue_calc(inputsyield2[2], 6.70)
+exp2 = expenses_calc(inputsyield2[1], 139.0)
+rev2/exp2
+rev2-exp2
+
+
+function revminusexp(profit, ymax, p::Float64=6.70, c::Float64=139.0)
+    return ((ymax*p - profit)/(2*c))^2
+end
+
+revminusexp(100, 120)
+
+let 
+    ymaxrange= 120.0:0.1:174.0
+    data100 = [revminusexp(100, ymax) for ymax in ymaxrange]
+    data150 = [revminusexp(150, ymax) for ymax in ymaxrange]
+    test = figure()
+    plot(data100, ymaxrange, color = "blue")
+    plot(data150, ymaxrange, color = "red")
     return test
 end
