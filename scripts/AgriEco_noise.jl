24376e3bc5d197c9352789164c2543e6d760e21a
chrisgg <chris@greyson-gaito.co.uk>
2023-03-13 17:37:15 -0400

    coding expectedprofits

--- scripts/AgriEco_noise.jl
+++ scripts/AgriEco_noise.jl
@@ -1,8 +1,24 @@
 include("packages.jl")
 include("AgriEco_commoncode.jl")
 
-# normal distribution to bimodal
-# https://stats.stackexchange.com/questions/128912/possible-to-morph-a-bimodal-distribution-into-a-normal-distribution-slowly
+function distribution_prob_convert(histogramdata)
+    sumcounts = sum(histogramdata.weights)
+    probdata = zeros(length(histogramdata.weights))
+    for bini in eachindex(probdata)
+        probdata[bini] = histogramdata.weights[bini]/sumcounts
+    end
+    return probdata
+end
+
+function profitsdata(yielddata, inputsdata, pdata, cdata)
+    profitsdata = zeros(length(yielddata))
+    for i in 1:length(yielddata)
+        rev = revenue_calc(yielddata[i], pdata[i])
+        exp = expenses_calc(inputsdata[i], cdata[i])
+        profitsdata[i] = rev - exp
+    end
+    return profitsdata
+end
 
 ###rev/exp
 #noise on disturbance
@@ -15,28 +31,36 @@ function expenses_calc(inputs, c)
     return inputs * c
 end
 
-function yield_noise(par,σ, corr, reps)
-    inputsyield = maxprofitIII_vals(par)
-    noise = noise_creation(inputsyield[2], σ, corr, reps, seed)
-
-
-function distribution_prob_convert(histogramdata)
-    sumcounts = sum(histogramdata.weights)
-    probdata = zeros(length(histogramdata.weights))
-    for bini in eachindex(probdata)
-        probdata[bini] = histogramdata.weights[bini]/sumcounts
-    end
-    return probdata
-end
-
-function expectedterminalassets(distributiondata, numbins)
+function expectedprofits(distributiondata, numbins)
     histogramdata = fit(Histogram, distributiondata, nbins=numbins)
     probdata = distribution_prob_convert(histogramdata)
-    expectedassets = 0
+    expectedprofits = 0
     for bini in eachindex(probdata)
         midpoint = histogramdata.edges[1][bini]+step(histogramdata.edges[1])/2
-        expectedassets += midpoint*probdata[bini]
+        expectedprofits += midpoint*probdata[bini]
     end
-    return expectedassets
+    return expectedprofits
 end
+
+function expectedprofits_yieldnoise(ymaxval, revexpratio, σ, corrrange, len, reps)
+    y0val = calc_y0(revexpratio, ymaxval, FarmBasePar().c, FarmBasePar().p)
+    newpar = FarmBasePar(ymax=ymaxval, y0=y0val)
+    inputsyield = maxprofitIII_vals(newpar)
+    avexpectedprofitsdata = zeros(length(corrrange))
+    @threads for ri in eachindex(corrrange)
+        expectedprofitsdata = zeros(reps)
+        for i in 1:reps
+            yielddata = noise_creation(inputsyield[2], σ, corrrange[ri], len, i)
+            inputsdata = repeat([inputsyield[1]], len)
+            pdata = repeat([newpar.p], len)
+            cdata = repeat([newpar.c], len)
+            expectedprofitsdata[i] = expectedprofits(profitsdata(yielddata, inputsdata, pdata, cdata), 25)
+        end
+        avexpectedprofitsdata[ri] = mean(expectedprofitsdata)
+    end
+    return avexpectedprofitsdata
+end
+
+
+expectedprofits_yieldnoise(120, 1.33, 0.1, 0.1:0.1:0.9, 50, 10)
 
