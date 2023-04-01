e5c42ddaa48e043f94cc8f91dc72c9fe3cdf9b81
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2023-04-01 16:22:42 -0400

    tried profit shortfall analysis

--- scripts/AgriEco_positivefeedbacks.jl
+++ scripts/AgriEco_positivefeedbacks.jl
@@ -297,89 +297,73 @@ let
     tight_layout()
     return rednoise_var_exptermassets
 end 
- #CV does not differ between high ymax and low ymax - WHY?
-
 
 #TERMINAL ASSETS SHORTFALL 
+function count_shortfall(distributiondata, shortfallval)
+    number = 0
+    for i in eachindex(distributiondata)
+        if distributiondata[i] < shortfallval
+            number +=1
+        end
+    end
+    return number
+end
 
+function termassetsshortfall_yield_rednoise(ymaxval, revexpratio, shortfallval, interestpar, yielddisturbance_sd, corrrange, maxyears, reps)
+    y0val = calc_y0(revexpratio, ymaxval, FarmBasePar().c, FarmBasePar().p)
+    newbasepar = FarmBasePar(ymax=ymaxval, y0=y0val)
+    inputsyield = maxprofitIII_vals(newbasepar)
+    data=zeros(length(corrrange), 4)
+    @threads for ri in eachindex(corrrange)
+        noisepar = NoisePar(yielddisturbed_σ = yielddisturbance_sd, yielddisturbed_r = corrrange[ri])
+        termassetsdata_NL = terminalassets_distribution("with", "yield", inputsyield, newbasepar, noisepar, interestpar, maxyears, reps)
+        termassetsdata_woNL = terminalassets_distribution("without", "yield", inputsyield, newbasepar, noisepar, interestpar, maxyears, reps)
+        termassetsshortfalldata_NL = count_shortfall(termassetsdata_NL, shortfallval)
+        termassetsshortfalldata_woNL = count_shortfall(termassetsdata_woNL, shortfallval)
+        data[ri,1] = corrrange[ri]
+        data[ri,2] = termassetsshortfalldata_NL
+        data[ri,3] = termassetsshortfalldata_woNL
+        data[ri,4] = termassetsshortfalldata_NL/termassetsshortfalldata_woNL
+    end
+    return data
+end
 
-# function exptermassets_revexpcon_data(noiselocation, revexpratio, ymaxrange, noisepar, interestpar, maxyears, reps, numbins, pval::Float64 =  6.70, cval::Float64 = 139.0)
-#     data=zeros(length(ymaxrange), 2)
-#     @threads for ymaxi in eachindex(ymaxrange)
-#         y0val = calc_y0(revexpratio, ymaxrange[ymaxi], cval, pval)
-#         par = FarmBasePar(ymax = ymaxrange[ymaxi], y0 = y0val, c = cval, p = pval)
-#         distributiondata = whitenoise_distribution(noiselocation, par, noisepar, interestpar, maxyears, reps)
-#         data[ymaxi, 1] = ymaxrange[ymaxi]
-#         data[ymaxi, 2] = expectedterminalassets(distributiondata, numbins)
-#     end
-#     return data
-# end 
-# #do I need if minimum(filter(!isnan, [margcostIII(I, par) for I in Irange])) >= par.p #|| minimum(filter(!isnan, [avvarcostIII(I, par) for I in Irange])) >= par.p
-
-# let 
-#     data133 = exptermassets_revexpcon_data("yield", 1.33, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
-#     data110 = exptermassets_revexpcon_data("yield", 1.10, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
-#     data100 = exptermassets_revexpcon_data("yield", 1.00, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
-#     data095 = exptermassets_revexpcon_data("yield", 0.95, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
-#     test = figure()
-#     plot(data133[:,1], data133[:,2], color="blue", label="Rev/Exp = 1.33")
-#     plot(data110[:,1], data110[:,2], color="red", label="Rev/Exp = 1.10")
-#     plot(data100[:,1], data100[:,2], color="purple", label="Rev/Exp =1.00")
-#     plot(data095[:,1], data095[:,2], color="orange", label="Rev/Exp = 0.95")
-#     xlabel("Ymax")
-#     ylabel("Expected terminal assets")
-#     legend()
-#     return test
-# end
-
-# let 
-#     data133 = exptermassets_revexpcon_data("none", 1.33, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
-#     data110 = exptermassets_revexpcon_data("none", 1.10, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
-#     data100 = exptermassets_revexpcon_data("none", 1.00, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
-#     data095 = exptermassets_revexpcon_data("none", 0.95, 120.0:1.0:180.0, NoisePar(), InterestPar(), 50, 100, 25)
-#     test = figure()
-#     plot(data133[:,1], data133[:,2], color="blue", label="Rev/Exp = 1.33")
-#     plot(data110[:,1], data110[:,2], color="red", label="Rev/Exp = 1.10")
-#     plot(data100[:,1], data100[:,2], color="purple", label="Rev/Exp =1.00")
-#     plot(data095[:,1], data095[:,2], color="orange", label="Rev/Exp = 0.95")
-#     xlabel("Ymax")
-#     ylabel("Expected terminal assets")
-#     legend()
-#     return test
-# end
-
-# #why is yield, price, and cost basically the same result?**** No noise anywhere makes the same figure
-# #because along the gradient of low to high ymax we are changing the absolute profits but keeping the relative profits constant ***
- 
-
-# calc_y0(1.33, 174, 139.0, 6.70)
-# calc_y0(1.33, 120, 139.0, 6.70)
-
-# inputsyield1 = maxprofitIII_vals(FarmBasePar(ymax=174.0, y0=calc_y0(1.33, 174, 139.0, 6.70), p=6.70, c=139.0))
-# rev1 = revenue_calc(inputsyield1[2], 6.70)
-# exp1 = expenses_calc(inputsyield1[1], 139.0)
-# rev1/exp1
-# rev1-exp1
-
-# inputsyield2 = maxprofitIII_vals(FarmBasePar(ymax=120.0, y0=calc_y0(1.33, 120.0, 139.0, 6.70), p=6.70, c=139.0))
-# rev2 = revenue_calc(inputsyield2[2], 6.70)
-# exp2 = expenses_calc(inputsyield2[1], 139.0)
-# rev2/exp2
-# rev2-exp2
-
-
-# function revminusexp(profit, ymax, p::Float64=6.70, c::Float64=139.0)
-#     return ((ymax*p - profit)/(2*c))^2
-# end
-
-# revminusexp(100, 120)
-
-# let 
-#     ymaxrange= 120.0:0.1:174.0
-#     data100 = [revminusexp(100, ymax) for ymax in ymaxrange]
-#     data150 = [revminusexp(150, ymax) for ymax in ymaxrange]
-#     test = figure()
-#     plot(data100, ymaxrange, color = "blue")
-#     plot(data150, ymaxrange, color = "red")
-#     return test
-# end
+let 
+    sd = 20
+    corrrange = 0.0:0.01:0.85
+    reps = 4000
+    shortfallval = 1000
+    highymax_133 = termassetsshortfall_yield_rednoise(170, 1.33, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    highymax_115 = termassetsshortfall_yield_rednoise(170, 1.15, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    highymax_100 = termassetsshortfall_yield_rednoise(170, 1.00, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    medymax_133 = termassetsshortfall_yield_rednoise(140, 1.33, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    medymax_115 = termassetsshortfall_yield_rednoise(140, 1.15, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    medymax_100 = termassetsshortfall_yield_rednoise(140, 1.00, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    lowymax_133 = termassetsshortfall_yield_rednoise(120, 1.33, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    lowymax_115 = termassetsshortfall_yield_rednoise(120, 1.15, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    lowymax_100 = termassetsshortfall_yield_rednoise(130, 1.00, shortfallval, InterestPar(), sd, corrrange, 50, reps)
+    rednoise_var_exptermassets = figure()    
+    subplot(3,1,1)
+    plot(highymax_133[:,1], highymax_133[:,4], color="blue", label="High ymax")
+    plot(medymax_133[:,1], medymax_133[:,4], color="red", label="Med ymax")
+    plot(lowymax_133[:,1], lowymax_133[:,4], color="purple", label="Low ymax")
+    xlabel("Autocorrelation")
+    ylabel("ShortwNL/ShortwoNL")
+    title("Rev/Exp = 1.33")
+    subplot(3,1,2)
+    plot(highymax_115[:,1], highymax_115[:,4], color="blue", label="High ymax")
+    plot(medymax_115[:,1], medymax_115[:,4], color="red", label="Med ymax")
+    plot(lowymax_115[:,1], lowymax_115[:,4], color="purple", label="Low ymax")
+    xlabel("Autocorrelation")
+    ylabel("ShortwNL/ShortwoNL")
+    title("Rev/Exp = 1.15")
+    subplot(3,1,3)
+    plot(highymax_100[:,1], highymax_100[:,4], color="blue", label="High ymax")
+    plot(medymax_100[:,1], medymax_100[:,4], color="red", label="Med ymax")
+    plot(lowymax_100[:,1], lowymax_100[:,4], color="purple", label="Low ymax")
+    xlabel("Autocorrelation")
+    ylabel("ShortwNL/ShortwoNL")
+    title("Rev/Exp = 1.10")
+    tight_layout()
+    return rednoise_var_exptermassets
+end 
