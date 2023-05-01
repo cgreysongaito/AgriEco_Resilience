d1979fe94bc46f57dba3570b76e03f7d375f0f4e
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2023-05-01 15:16:25 -0400

    cleaning of code after decisions about focus and CV

--- scripts/AgriEco_positivefeedbacks.jl
+++ scripts/AgriEco_positivefeedbacks.jl
@@ -33,99 +33,6 @@ function simulation_NLposfeed(NL, basedata, interestpar)
     return assetsdebt
 end
 
-function terminalassets_distribution(NL, inputsyield, economicpar, noisepar, interestpar, maxyears, reps)
-    assetsdebtdata =  zeros(reps)
-    for i in 1:reps
-        basedata = yieldnoise_createdata(inputsyield, economicpar, noisepar, maxyears, i)
-        simdata = simulation_NLposfeed(NL, basedata, interestpar)
-        assetsdebtdata[i] = simdata[end]
-    end
-    return assetsdebtdata
-end
-
-function terminalassets_posfeed_rednoise_dataset(ymaxy0vals, economicpar, interestpar, yielddisturbance_sd, corrrange, maxyears, reps)
-    defaultinputsyield = maxprofitIII_vals(ymaxy0vals[1],ymaxy0vals[2], economicpar)
-    data = Array{Vector{Float64}}(undef,length(corrrange), 2)
-    @threads for ri in eachindex(corrrange)
-        noisepar = NoisePar(yielddisturbed_σ = yielddisturbance_sd, yielddisturbed_r = corrrange[ri])
-        data[ri, 1] = terminalassets_distribution("with", defaultinputsyield, economicpar, noisepar, interestpar, maxyears, reps)
-        data[ri, 2] = terminalassets_distribution("without", defaultinputsyield, economicpar, noisepar, interestpar, maxyears, reps)
-    end
-    return hcat(corrrange, data)
-end
-
-
-
-# let
-#     highymax_133_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(170, 1.33, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/highymax_133_posfeed_data.csv"), highymax_133_posfeed_data)
-#     medymax_133_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(140, 1.33, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/medymax_133_posfeed_data.csv"), medymax_133_posfeed_data)
-#     lowymax_133_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(120, 1.33, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/lowymax_133_posfeed_data.csv"), lowymax_133_posfeed_data)
-# end
-
-# let
-#     highymax_115_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(170, 1.15, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/highymax_115_posfeed_data.csv"), highymax_115_posfeed_data)
-#     medymax_115_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(140, 1.15, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/medymax_115_posfeed_data.csv"), medymax_115_posfeed_data)
-#     lowymax_115_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(120, 1.15, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/lowymax_115_posfeed_data.csv"), lowymax_115_posfeed_data)
-# end
-
-# let
-#     highymax_108_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(170, 1.08, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/highymax_108_posfeed_data.csv"), highymax_108_posfeed_data)
-#     medymax_108_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(140, 1.08, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/medymax_108_posfeed_data.csv"), medymax_108_posfeed_data)
-#     lowymax_108_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(130, 1.08, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/lowymax_108_posfeed_data.csv"), lowymax_108_posfeed_data)
-# end
-
-lowrevexpratio = 1.08
-lowymaxvalue = 140
-rise = 10
-run = 0.02
-sd_posfeed = 20
-corrrange_posfeed = 0.0:0.01:0.85
-maxyears_posfeed = 50
-reps_posfeed = 1000
-
-#ymax and y0 both change (general response)
-let
-    vals = calcymaxy0vals("neither", lowymaxvalue, [1.08,1.15,1.33], rise, run, EconomicPar())
-    changeboth_108_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[1,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/changeboth_108_posfeed_data.csv"), changeboth_108_posfeed_data)
-    changeboth_115_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[2,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/changeboth_115_posfeed_data.csv"), changeboth_115_posfeed_data)
-    changeboth_133_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[3,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/changeboth_133_posfeed_data.csv"), changeboth_133_posfeed_data)
-end
-
-#Constrain ymax
-let
-    vals = calcymaxy0vals("ymax", lowymaxvalue, [1.08,1.15,1.33], rise, run, EconomicPar())
-    constrainymax_108_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[1,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/constrainymax_108_posfeed_data.csv"), constrainymax_108_posfeed_data)
-    constrainymax_115_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[2,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/constrainymax_115_posfeed_data.csv"), constrainymax_115_posfeed_data)
-    constrainymax_133_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[3,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/constrainymax_133_posfeed_data.csv"), constrainymax_133_posfeed_data)
-end
-
-#Constrain y0
-let
-    vals = calcymaxy0vals("y0", lowymaxvalue, [1.08,1.15,1.33], rise, run, EconomicPar())
-    constrainy0_108_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[1,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/constrainy0_108_posfeed_data.csv"), constrainy0_108_posfeed_data)
-    constrainy0_115_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[2,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/constrainy0_115_posfeed_data.csv"), constrainy0_115_posfeed_data)
-    constrainy0_133_posfeed_data = prepDataFrame(terminalassets_posfeed_rednoise_dataset(vals[3,:], EconomicPar(), InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/constrainy0_133_posfeed_data.csv"), constrainy0_133_posfeed_data)  
-end
-
-#CV
 function terminalassets_distribution_CV(NL, inputsyield, economicpar, noiseparCV, interestpar, maxyears, reps)
     assetsdebtdata =  zeros(reps)
     for i in 1:reps
@@ -189,67 +96,16 @@ let
     CSV.write(joinpath(abpath(), "data/constrainy0_133_posfeed_data_CV.csv"), constrainy0_133_posfeed_data_CV)  
 end
 
-# let
-#     highymax_133_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(170, 1.33, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/highymax_133_posfeed_data_CV.csv"), highymax_133_posfeed_data_CV)
-#     medymax_133_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(140, 1.33, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/medymax_133_posfeed_data_CV.csv"), medymax_133_posfeed_data_CV)
-#     lowymax_133_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(120, 1.33, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/lowymax_133_posfeed_data_CV.csv"), lowymax_133_posfeed_data_CV)
+# #Rev-exp (absolute)
+# function terminalassets_posfeed_rednoise_dataset_abs(ymaxval, revexpabs, interestpar, yielddisturbance_sd, corrrange, maxyears, reps)
+#     y0val = calc_y0_abs(revexpabs, ymaxval, FarmBasePar().c, FarmBasePar().p)
+#     newbasepar = FarmBasePar(ymax=ymaxval, y0=y0val)
+#     defaultinputsyield = maxprofitIII_vals(newbasepar)
+#     data = Array{Vector{Float64}}(undef,length(corrrange), 2)
+#     @threads for ri in eachindex(corrrange)
+#         noisepar = NoisePar(yielddisturbed_σ = yielddisturbance_sd, yielddisturbed_r = corrrange[ri])
+#         data[ri, 1] = terminalassets_distribution("with", defaultinputsyield, newbasepar, noisepar, interestpar, maxyears, reps)
+#         data[ri, 2] = terminalassets_distribution("without", defaultinputsyield, newbasepar, noisepar, interestpar, maxyears, reps)
+#     end
+#     return hcat(corrrange, data)
 # end
-
-# let
-#     highymax_115_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(170, 1.15, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/highymax_115_posfeed_data_CV.csv"), highymax_115_posfeed_data_CV)
-#     medymax_115_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(140, 1.15, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/medymax_115_posfeed_data_CV.csv"), medymax_115_posfeed_data_CV)
-#     lowymax_115_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(120, 1.15, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/lowymax_115_posfeed_data_CV.csv"), lowymax_115_posfeed_data_CV)
-# end
-
-# let
-#     highymax_108_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(170, 1.08, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/highymax_108_posfeed_data_CV.csv"), highymax_108_posfeed_data_CV)
-#     medymax_108_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(140, 1.08, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/medymax_108_posfeed_data_CV.csv"), medymax_108_posfeed_data_CV)
-#     lowymax_108_posfeed_data_CV = prepDataFrame(terminalassets_posfeed_rednoise_dataset_CV(130, 1.08, InterestPar(), CV_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-#     CSV.write(joinpath(abpath(), "data/lowymax_108_posfeed_data_CV.csv"), lowymax_108_posfeed_data_CV)
-# end
-
-
-#Rev-exp (absolute)
-function terminalassets_posfeed_rednoise_dataset_abs(ymaxval, revexpabs, interestpar, yielddisturbance_sd, corrrange, maxyears, reps)
-    y0val = calc_y0_abs(revexpabs, ymaxval, FarmBasePar().c, FarmBasePar().p)
-    newbasepar = FarmBasePar(ymax=ymaxval, y0=y0val)
-    defaultinputsyield = maxprofitIII_vals(newbasepar)
-    data = Array{Vector{Float64}}(undef,length(corrrange), 2)
-    @threads for ri in eachindex(corrrange)
-        noisepar = NoisePar(yielddisturbed_σ = yielddisturbance_sd, yielddisturbed_r = corrrange[ri])
-        data[ri, 1] = terminalassets_distribution("with", defaultinputsyield, newbasepar, noisepar, interestpar, maxyears, reps)
-        data[ri, 2] = terminalassets_distribution("without", defaultinputsyield, newbasepar, noisepar, interestpar, maxyears, reps)
-    end
-    return hcat(corrrange, data)
-end
-
-sd_posfeed = 20
-corrrange_posfeed = 0.0:0.01:0.85
-maxyears_posfeed = 50
-reps_posfeed = 1000
-
-let
-    highymax_225_posfeed_data_abs = prepDataFrame(terminalassets_posfeed_rednoise_dataset_abs(170, 225, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/highymax_225_posfeed_data_abs.csv"), highymax_225_posfeed_data_abs)
-    medymax_225_posfeed_data_abs = prepDataFrame(terminalassets_posfeed_rednoise_dataset_abs(140, 225, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/medymax_225_posfeed_data_abs.csv"), medymax_225_posfeed_data_abs)
-    lowymax_225_posfeed_data_abs = prepDataFrame(terminalassets_posfeed_rednoise_dataset_abs(120, 225, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/lowymax_225_posfeed_data_abs.csv"), lowymax_225_posfeed_data_abs)
-end
-
-let
-    highymax_75_posfeed_data_abs = prepDataFrame(terminalassets_posfeed_rednoise_dataset_abs(170, 75, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/highymax_75_posfeed_data_abs.csv"), highymax_75_posfeed_data_abs)
-    medymax_75_posfeed_data_abs = prepDataFrame(terminalassets_posfeed_rednoise_dataset_abs(140, 75, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/medymax_75_posfeed_data_abs.csv"), medymax_75_posfeed_data_abs)
-    lowymax_75_posfeed_data_abs = prepDataFrame(terminalassets_posfeed_rednoise_dataset_abs(130, 75, InterestPar(), sd_posfeed, corrrange_posfeed, maxyears_posfeed, reps_posfeed))
-    CSV.write(joinpath(abpath(), "data/lowymax_75_posfeed_data_abs.csv"), lowymax_75_posfeed_data_abs)
-end
--- scripts/AgriEco_timedelay.jl
+++ scripts/AgriEco_timedelay.jl
@@ -10,23 +10,6 @@ function delayedinputs(defaultinputs, lastyrsactualyield, lastyrsprojectedyield,
     end
 end
 
-function NLtimedelay_data(defaultinputsyield, ymax, y0, yearsdelay, noisepar, minfraction, maxyears, seed)
-    basenoise = noise_creation(0.0, noisepar.yielddisturbed_σ, noisepar.yielddisturbed_r, maxyears+yearsdelay, seed)
-    inputsdata = zeros(maxyears+yearsdelay)
-    yielddata = zeros(maxyears+yearsdelay)
-    for i in 1:yearsdelay
-        inputsdata[i] = defaultinputsyield[1]
-        yielddata[i] = defaultinputsyield[2] + basenoise[i]
-    end
-    for i in yearsdelay+1:maxyears+yearsdelay
-        previousyrsactualyield = mean(yielddata[i-yearsdelay:i-1])
-        previousyrsprojectedyield = mean([yieldIII(inputs, ymax, y0) for inputs in inputsdata[i-yearsdelay:i-1]])
-        inputsdata[i] = delayedinputs(defaultinputsyield[1], previousyrsactualyield, previousyrsprojectedyield, minfraction)
-        yielddata[i] = yieldIII(inputsdata[i], ymax, y0) + basenoise[i]
-    end
-    return hcat(inputsdata[yearsdelay+1:maxyears+yearsdelay], convert_neg_zero(yielddata[yearsdelay+1:maxyears+yearsdelay]))
-end
-
 function simulation_NLtimedelay(basedata, economicpar)
     maxyears = size(basedata,1)
     assetsdebt = zeros(maxyears+1)
@@ -36,26 +19,6 @@ function simulation_NLtimedelay(basedata, economicpar)
         assetsdebt[yr] = assetsdebt[yr-1] + (revenue - expenses)
     end
     return assetsdebt
-end
-
-function terminalassets_distribution_NLtimedelay(NL, defaultinputsyield, ymax, y0, yearsdelay, economicpar, noisepar, minfraction, maxyears, reps)
-    assetsdebtdata =  zeros(reps)
-    if NL == "with"
-        for i in 1:reps
-            basedataNL = NLtimedelay_data(defaultinputsyield, ymax, y0, yearsdelay, noisepar, minfraction, maxyears, i)
-            simdata = simulation_NLtimedelay(basedataNL, economicpar)
-            assetsdebtdata[i] = simdata[end]
-        end
-    elseif NL == "without"
-        for i in 1:reps
-            basedatawoNL = hcat(repeat([defaultinputsyield[1]], maxyears), yielddisturbed(defaultinputsyield, noisepar, maxyears, i))
-            simdata = simulation_NLtimedelay(basedatawoNL, economicpar)
-            assetsdebtdata[i] = simdata[end]
-        end
-    else
-        error("NL should be either with or without")
-    end
-    return assetsdebtdata
 end
 
 ## CV
@@ -185,73 +148,16 @@ let
     CSV.write(joinpath(abpath(), "data/constrainy0_133_timedelay_data_CV_6.csv"), constrainy0_133_timedelay_data_CV_6)  
 end
 
-# #Rev/Exp = 1.33
-# let
-#     highymax_133_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(170, 1.33, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/highymax_133_timedelay_data_CV.csv"), highymax_133_timedelay_data_CV)
-#     medymax_133_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(140, 1.33, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/medymax_133_timedelay_data_CV.csv"), medymax_133_timedelay_data_CV)
-#     lowymax_133_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(120, 1.33, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/lowymax_133_timedelay_data_CV.csv"), lowymax_133_timedelay_data_CV)
+# #Rev-exp
+# function terminalassets_timedelay_rednoise_dataset_abs(ymaxval, revexpabs, yielddisturbance_sd, corrrange, yearsdelay, minfraction, maxyears, reps)
+#     y0val = calc_y0_abs(revexpabs, ymaxval, FarmBasePar().c, FarmBasePar().p)
+#     newbasepar = FarmBasePar(ymax=ymaxval, y0=y0val)
+#     defaultinputsyield = maxprofitIII_vals(newbasepar)
+#     data = Array{Vector{Float64}}(undef,length(corrrange), 2)
+#     @threads for ri in eachindex(corrrange)
+#         noisepar = NoisePar(yielddisturbed_σ = yielddisturbance_sd, yielddisturbed_r = corrrange[ri])
+#         data[ri, 1] = terminalassets_distribution_NLtimedelay("with", defaultinputsyield, yearsdelay, newbasepar, noisepar, minfraction, maxyears, reps)
+#         data[ri, 2] = terminalassets_distribution_NLtimedelay("without", defaultinputsyield, yearsdelay, newbasepar, noisepar, minfraction, maxyears, reps)
+#     end
+#     return hcat(corrrange, data)
 # end
-
-# #Rev/Exp = 1.15
-# let
-#     highymax_115_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(170, 1.15, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/highymax_115_timedelay_data_CV.csv"), highymax_115_timedelay_data_CV)
-#     medymax_115_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(140, 1.15, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/medymax_115_timedelay_data_CV.csv"), medymax_115_timedelay_data_CV)
-#     lowymax_115_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(120, 1.15, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/lowymax_115_timedelay_data_CV.csv"), lowymax_115_timedelay_data_CV)
-# end
-
-# #Rev/Exp = 1.08
-# let
-#     highymax_108_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(170, 1.08, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/highymax_108_timedelay_data_CV.csv"), highymax_108_timedelay_data_CV)
-#     medymax_108_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(140, 1.08, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/medymax_108_timedelay_data_CV.csv"), medymax_108_timedelay_data_CV)
-#     lowymax_108_timedelay_data_CV = prepDataFrame(terminalassets_timedelay_rednoise_dataset_CV(130, 1.08, CV_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-#     CSV.write(joinpath(abpath(), "data/lowymax_108_timedelay_data_CV.csv"), lowymax_108_timedelay_data_CV)
-# end
-
-#Rev-exp
-function terminalassets_timedelay_rednoise_dataset_abs(ymaxval, revexpabs, yielddisturbance_sd, corrrange, yearsdelay, minfraction, maxyears, reps)
-    y0val = calc_y0_abs(revexpabs, ymaxval, FarmBasePar().c, FarmBasePar().p)
-    newbasepar = FarmBasePar(ymax=ymaxval, y0=y0val)
-    defaultinputsyield = maxprofitIII_vals(newbasepar)
-    data = Array{Vector{Float64}}(undef,length(corrrange), 2)
-    @threads for ri in eachindex(corrrange)
-        noisepar = NoisePar(yielddisturbed_σ = yielddisturbance_sd, yielddisturbed_r = corrrange[ri])
-        data[ri, 1] = terminalassets_distribution_NLtimedelay("with", defaultinputsyield, yearsdelay, newbasepar, noisepar, minfraction, maxyears, reps)
-        data[ri, 2] = terminalassets_distribution_NLtimedelay("without", defaultinputsyield, yearsdelay, newbasepar, noisepar, minfraction, maxyears, reps)
-    end
-    return hcat(corrrange, data)
-end
-
-sd_timedelay = 20
-corrrange_timedelay = 0.0:0.01:0.85
-yearsdelay = 3
-minfraction_timedelay = 0.2
-maxyears_timedelay = 50
-reps_timedelay = 1000
-
-#Rev-Exp = 225
-let
-    highymax_225_timedelay_data_abs = prepDataFrame(terminalassets_timedelay_rednoise_dataset_abs(170, 225, sd_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-    CSV.write(joinpath(abpath(), "data/highymax_225_timedelay_data_abs.csv"), highymax_225_timedelay_data_abs)
-    medymax_225_timedelay_data_abs = prepDataFrame(terminalassets_timedelay_rednoise_dataset_abs(140, 225, sd_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-    CSV.write(joinpath(abpath(), "data/medymax_225_timedelay_data_abs.csv"), medymax_225_timedelay_data_abs)
-    lowymax_225_timedelay_data_abs = prepDataFrame(terminalassets_timedelay_rednoise_dataset_abs(120, 225, sd_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-    CSV.write(joinpath(abpath(), "data/lowymax_225_timedelay_data_abs.csv"), lowymax_225_timedelay_data_abs)
-end
-
-#Rev-Exp = 75
-let
-    highymax_75_timedelay_data_abs = prepDataFrame(terminalassets_timedelay_rednoise_dataset_abs(170, 75, sd_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-    CSV.write(joinpath(abpath(), "data/highymax_75_timedelay_data_abs.csv"), highymax_75_timedelay_data_abs)
-    medymax_75_timedelay_data_abs = prepDataFrame(terminalassets_timedelay_rednoise_dataset_abs(140, 75, sd_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-    CSV.write(joinpath(abpath(), "data/medymax_75_timedelay_data_abs.csv"), medymax_75_timedelay_data_abs)
-    lowymax_75_timedelay_data_abs = prepDataFrame(terminalassets_timedelay_rednoise_dataset_abs(130, 75, sd_timedelay, corrrange_timedelay, yearsdelay, minfraction_timedelay, maxyears_timedelay, reps_timedelay))
-    CSV.write(joinpath(abpath(), "data/lowymax_75_timedelay_data_abs.csv"), lowymax_75_timedelay_data_abs)
-end
